a334dfbac6fd5bd0e59bd8dcf52ef55233efd11f
Laker Turner <git@laker.gay>
2022-12-26 17:14:10 +0000

    refactor and delete stuff

--- main.js
+++ main.js
@@ -1,6 +1,4 @@
-
 // pronouns.page integration
-
 function pronouns(data) {
     document.getElementById("pronouns").innerHTML = '('+data.profiles.en.pronouns[0].value+')';
 }
@@ -8,25 +6,3 @@ function pronouns(data) {
 fetch('https://en.pronouns.page/api/profile/get/laker?version=2')
     .then((response) => response.json())
     .then((data) => pronouns(data));
-
-
-// neatnik/typo
-document.addEventListener('DOMContentLoaded', function() { 
-  var element = document.getElementById('typo');
-  var text = element.innerHTML;
-  typo(element, text);
-});
-
-// pgp copy
-
-function pgpGet() {
-  // Get the text field
-  var copyText = document.getElementById("myInput");
-
-  // Select the text field
-  copyText.select();
-  copyText.setSelectionRange(0, 99999); // For mobile devices
-
-   // Copy the text inside the text field
-  navigator.clipboard.writeText(copyText.value);
-}
