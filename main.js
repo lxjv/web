889e6f077abe634e8ecc8d535d26f47935bb17f1
Laker J.V Turner <git@laker.gay>
2023-01-06 12:28:01 +0000

    updates

--- main.js
+++ main.js
@@ -2,7 +2,6 @@
 function pronouns(data) {
     document.getElementById("pronouns").innerHTML = '('+data.profiles.en.pronouns[0].value+')';
 }
-
 fetch('https://en.pronouns.page/api/profile/get/laker?version=2')
     .then((response) => response.json())
     .then((data) => pronouns(data));
