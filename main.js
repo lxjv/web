251be055ed973bc52783696fa8d1c02f619636f5
Laker Turner <git@laker.gay>
2023-03-03 23:05:49 +0000

    lets goooo

--- main.js
+++
@@ -1,7 +0,0 @@
-// pronouns.page integration
-function pronouns(data) {
-    document.getElementById("pronouns").innerHTML = '('+data.profiles.en.pronouns[0].value+')';
-}
-fetch('https://en.pronouns.page/api/profile/get/laker?version=2')
-    .then((response) => response.json())
-    .then((data) => pronouns(data));
